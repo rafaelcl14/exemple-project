40dfc458e689ea100d54ba64271eba1b16e7cb3c
Rafael Carvalho Rosina <39815593+rafaelcl14@users.noreply.github.com>
2021-07-06 22:12:17 -0400

    Project Exemple Landing Page

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import '../style/button.scss';
+import {ButtonHTMLAttributes} from 'react';
+
+
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+
+export function Button(props: ButtonProps) {
+    return (
+        <button className="button" {...props}/>
+    )
+}
+
